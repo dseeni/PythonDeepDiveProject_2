abdffcf50d4637634a5cb4d7aef7d88aad33dda0
Deepak Seeni <dseeni@gmail.com>
2019-05-16 17:48:33 -0700

    def test_widget() pass//fail

--- Test_PDD2_Project2/test_widget.py
+++ Test_PDD2_Project2/test_widget.py
@@ -1,2 +1,7 @@
+
 # This is a test widget for the Automation Framework PyTest course
-# This is a test widget for the Automation Framework PyTest course
+def test_widget_functions_as_expected():
+    assert True
+
+def test_widget_fail():
+    assert False
