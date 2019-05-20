0f99832b8c917d79308c884a48f0655be77d6f79
Deepak Seeni <dseeni@gmail.com>
2019-05-19 22:14:25 -0700

    clean up comments

--- conftest.py
+++ conftest.py
@@ -12,7 +12,6 @@ def poly_objs():
 
     name = tuple(('poly{0}'.format(n)) for n in range(100, 100+len(n)))
 
-    # same as the for loop below
     polylist = [p.Poly(n, r) for n, r in nr]
     polydict = OrderedDict(zip(name, polylist))
 
@@ -24,6 +23,5 @@ def poly_objs():
     #           'edge_length =', v.edge_length,"\n",
     #           'area =', v.area
     #           )
-    return polydict     # unit test: __repr__
+    return polydict
 
-# print(poly_objs())
