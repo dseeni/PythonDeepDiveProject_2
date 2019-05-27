0d40221b0d86568f803468410cc5402b1d00f31a
Deepak Seeni <dseeni@gmail.com>
2019-05-27 00:38:48 -0700

    Add test_updating_key_makes_key_newest_item FAILED

--- tests/test_nrs_cache_global.py
+++ tests/test_nrs_cache_global.py
@@ -61,6 +61,7 @@ def test_global_cache_oldest_item(test_global_cache):
 
 
 def test_global_cache_newest_item(test_global_cache):
+
     assert ((test_global_cache.popitem(last=True)) ==
             ((70, 50, 0.1), {'apothem': [300], 'area': [600], 'interior angle': [300]}))
 
@@ -72,7 +73,8 @@ def test_existing_key_additional_calculated_properties(test_global_cache):
 
 def test_updating_key_makes_key_newest_item(test_global_cache):
     test_global_cache[(50, 50, .1)]['interior_angle'] = [60]
-
+    assert ((test_global_cache.popitem(last=True)) ==
+            ((50, 50, 0.1), {'apothem': [100], 'area': [400], 'interior_angle': [60]}))
 
 # TODO updating a key makes key recent item
 # TODO cache_size respects cache limit, old keys are discarded
