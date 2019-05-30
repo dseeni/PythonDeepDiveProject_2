d332f1b6a3c012e3836610c61fe910f342d4e29f
Deepak Seeni <dseeni@gmail.com>
2019-05-29 20:54:03 -0700

    Reorder function list in test_parameter_checker.py

--- tests/test_parameter_checker.py
+++ tests/test_parameter_checker.py
@@ -1,21 +1,5 @@
 from src import parameter_checker as pm
 
-# check_parameters tests
-def test_check_parameters_docstring():
-    assert pm.check_parameters.__doc__ is not None
-
-
-def test_check_parameters_callable():
-    assert (pm.check_parameters())
-
-
-# check_parameters(icount=10, clones=2, cloneamount=2, siderange=(3,10), radrange=(1,5), sig=2)
-def test_check_parameters_default_finalcount():
-    returnvalues = pm.check_parameters()
-    finalcount = (returnvalues[0] - returnvalues[1]) \
-    + (returnvalues[1] * returnvalues[2])
-    assert finalcount == 12
-
 
 # frange helper function tests
 def test_frange_docstring():
@@ -35,4 +19,20 @@ def test_frange_step_length():
 # test frange returns len of stop - start x
 def test_frange_output_length():
     assert len(list(pm.frange(1.5,5.5,1))) == 4
+
+
+# check_parameters tests
+def test_check_parameters_docstring():
+    assert pm.check_parameters.__doc__ is not None
+
+
+def test_check_parameters_callable():
+    assert (pm.check_parameters())
+
+# check_parameters(icount=10, clones=2, cloneamount=2, siderange=(3,10), radrange=(1,5), sig=2)
+def test_check_parameters_default_finalcount():
+    returnvalues = pm.check_parameters()
+    finalcount = (returnvalues[0] - returnvalues[1]) \
+    + (returnvalues[1] * returnvalues[2])
+    assert finalcount == 12
 
