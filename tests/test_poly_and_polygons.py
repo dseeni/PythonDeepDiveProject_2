d3dcebbee0e4734dd80b2d6051d39677b0b51fb6
Deepak Seeni <dseeni@gmail.com>
2019-05-30 01:19:01 -0700

    WIP test_poly_and_polygons.py coverage for Poly at 78%

--- tests/test_poly_and_polygons.py
+++ tests/test_poly_and_polygons.py
@@ -27,7 +27,7 @@ def test_polygon_specific_value():
     poly10 = pp.Poly(4, 1)
     poly11 = pp.Poly(4, 1)
     poly12 = pp.Poly(4, 1)
-    poly13 = pp.Poly(1000, 1)
+    poly13 = pp.Poly(1000, 1, 5)
     for i in range(9,14):
         eval("poly{0}.calcproperties".format(i))
     assert math.isclose(poly9.apothem, 1.732, rel_tol=.001, abs_tol=.001)
@@ -56,24 +56,24 @@ def test_polygon_calulate_properties_method():
 
 
 # Testing for Polygons Class #
-def test_polygons_repr(test_polygons):
-    # Polygons Class __repr__ test
-    assert str(test_polygons[0]) == 'Polygons(10,6.35)', '{0}'.format(polys)
-
-
-def test_polygons_max_efficencey_method(test_polygons):
-    # test the max efficencey formula
-    assert max([p.area/p.perimeter for p in test_polygons[1]]) == test_polygons[1].max_efficiency
-
-
-def test_polygons_sequence(test_polygons):
-    # Access the sequence type
-    assert test_polygons[0][1]
-
-
-def test_polygon_sequence_pi_area(test_polygons):
-    # calculate area of Pi
-    assert math.isclose(test_polygons[1][497].area, math.pi, abs_tol=.0001, rel_tol=.0001)
+# def test_polygons_repr(test_polygons):
+#     # Polygons Class __repr__ test
+#     assert str(test_polygons[0]) == 'Polygons(10,6.35)', '{0}'.format(polys)
+#
+#
+# def test_polygons_max_efficencey_method(test_polygons):
+#     # test the max efficencey formula
+#     assert max([p.area/p.perimeter for p in test_polygons[1]]) == test_polygons[1].max_efficiency
+#
+#
+# def test_polygons_sequence(test_polygons):
+#     # Access the sequence type
+#     assert test_polygons[0][1]
+#
+#
+# def test_polygon_sequence_pi_area(test_polygons):
+#     # calculate area of Pi
+#     assert math.isclose(test_polygons[1][497].area, math.pi, abs_tol=.0001, rel_tol=.0001)
 
     # print('pp.Polygons Class setitem method testing...')
     #
