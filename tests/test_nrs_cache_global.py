297fd1e4b8f0e7ae15f664d1dac753ed62f1378e
Deepak Seeni <dseeni@gmail.com>
2019-07-03 22:04:58 -0700

    Fix test_global_cache_getter_method_fifo

--- tests/test_nrs_cache_global.py
+++ tests/test_nrs_cache_global.py
@@ -3,6 +3,7 @@ from src.poly_and_polygons import NrsGlobalCache as NCacheGlobal
 from pytest import raises
 from src.poly_and_polygons import Poly, CacheGlobal
 
+
 # NrsGlobalCache docstring test
 def test_global_cache_docstring(test_global_cache):
     assert test_global_cache.__doc__ is not None
@@ -97,42 +98,22 @@ def test_global_cache_limit_respects_new_item(test_global_cache):
     test_global_cache.cache_limit = 1
     print('test_global_cache.cache_size = ', test_global_cache.cache_size)
     test_global_cache.setter((60, 60, .1), 'area', 100)
-    # print('test_global_cache.cache_size(test_global_cache.keys()) = ',
-    #       test_global_cache.cache_size(len(test_global_cache.keys())))
-    # print('test_global_cache.items() = ', test_global_cache.items())
     assert test_global_cache.cache_size == 1
 
 
-def test_global_cache_getter_method_lifo(test_global_cache):
+def test_global_cache_getter_method_fifo(test_global_cache):
     apothem = test_global_cache.getter((50, 50, .1), 'apothem')
     print('apothem = ', apothem)
     assert next(reversed(test_global_cache)) == (50, 50, .1)
     assert len(test_global_cache.key_view) == 3
     assert next(reversed(test_global_cache)) == (50, 50, .1)
-    p = Poly(5,2,3)
-    p1 = Poly(5,2,3)
-    p2 = Poly(5,3,3)
+    p = Poly(5, 2, 3)
+    p1 = Poly(5, 2, 3)
+    p2 = Poly(5, 3, 3)
     interior_angle = p.interior_angle, p1.interior_angle, p2.interior_angle
     print(interior_angle)
     keys = reversed(CacheGlobal)
-    assert next(keys) == (5,3,3)
-    assert next(keys) == (5,2,3)
+    assert next(keys) == (5, 3, 3)
+    assert next(keys) == (5, 2, 3)
     assert len(CacheGlobal) == 2
 
-
-
-
-# p = Poly(5,2,3)
-# p1 = Poly(5,2,3)
-#
-# # print(p.interior_angle)
-# # print(p1.interior_angle)
-# # print(p1.interior_angle)
-#
-# # print(p.area)
-# # print(p.area)
-# # print(p1.area)
-# # print(p1.area)
-# print(p.perimeter)
-# print(p.perimeter)
-# print(p1.perimeter)
